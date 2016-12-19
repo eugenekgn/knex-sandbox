e7f0e6e74f4ab9eee000b202dee399346f8fa427
Eugene <eugenekgn@gmail.com>
2016-12-18 21:57:26 -0500

    working sqlite

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+'use strict';
+
 const configuration = {
   client: 'sqlite3',
   connection: {
     filename: './book.sqlite'
-  }
+  },
+  useNullAsDefault: true
 };
 
-const
+const knex = require('knex')(configuration);
+
+knex.select('title', 'rating').from('book').asCallback((err, rows) => {
+    if (err) {
+      console.log(`Error ${erro}`);
+    }
+    console.log(rows);
+  });
+
+knex.destroy();
+
